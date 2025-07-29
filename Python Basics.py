443625440ebbbfa0f7d6ec1bdef3503d341ef182
koppojuTrinaini <130895365+koppojuTrinaini@users.noreply.github.com>
2025-07-29 21:54:59 +0530

    Add files via upload

---
+++ Python Basics.py
@@ -0,0 +1,49 @@
+# Very Easy
+
+# name =  "Chinnodaaaaa 💞✨"
+# print("My name is "+name)
+# age = 20
+# print(f"I am {age} years old")
+# x = 10
+# y = 3.5
+# print(f"Sum Of {x} and {y} is : {x+y}")
+
+# Askname = input("Enter your name : ")
+# print(f"Hello, {Askname}!")
+# Strnum = "123"
+# print(f"The String '{Strnum}' converted to integer {int(Strnum)}")
+
+#  Easy
+
+# a=int(input("Enter number a = "))
+# b=int(input("Enter number b = "))
+# print(f"Sum of {a} and {b} is : {a+b}")
+# print(f"Product of {a} and {b} is : {a*b}")
+
+# age = input("Enter Your AGE :")
+# convertage = int(age)
+# print (f"Age is converted to interger is {convertage}")
+# print (f"After adding 5 to the given AGE is '{convertage + 5}'")
+
+# a = 10
+# b = 10.5
+# c = "hello"
+# d = True
+# print(f"Type of {a} is {type(a)}")
+# print(f"Type of {b} is {type(b)}")
+# print(f"Type of {c} is {type(c)}")
+# print(f"Type of {d} is {type(d)}")
+
+# x = float(input("Enter a Float number : "))
+# print(f"The Float {x} is Converted to interger is {int(x)}")
+
+# u = input("Enter something: ")
+# if u.isdigit():
+#     print("It is a NUMBER")
+# else:
+#     print("It is NOT a NUMBER")
+
+# Medium
+
+fullname = "Chinnodaaaaaa"
+print(f"The Count of the {fullname} is {len(fullname)}")
