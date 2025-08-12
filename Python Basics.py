b3f1130140b9afebb1f134eafed2883d0aa12205
koppojuTrinaini <130895365+koppojuTrinaini@users.noreply.github.com>
2025-08-12 23:37:31 +0530

    Update Python Basics.py

--- Python Basics.py
+++ Python Basics.py
@@ -6,6 +6,7 @@
 # Convert the string "123" into an integer and print it.
 print("Very Easy✨")
 
+
 name =  "MiniCat"
 print("My name is "+name)
 age = 20
@@ -146,5 +147,6 @@ elif (e == '*'):
     print(n1*n2)
 else:
     print(n1-n2)
+
 
 
